d7538985be15bf5160c2f09ec79e97c0c469084b
Angie Moreno <angievalemd@gmail.com>
2023-05-31 23:13:02 -0500

    Version 3

---
+++ especialidad.js
@@ -0,0 +1,35 @@
+class FormEngine2{
+
+    async render(){
+
+        const response= await fetch("http://localhost:3000/formulario/Especialidad")
+        const form= await response.json()
+        
+        const formHtml= document.createElement("form")
+
+        formHtml.classList.add("container")
+
+        for( const [key, value] of Object.entries(form.properties)){
+
+            const label= document.createElement("label")
+            label.innerText=key
+            formHtml.appendChild(label)
+            const input= document.createElement("input")
+            input.name=key
+            input.classList.add("form-control")
+
+            if(value.type=="integer"){
+                input.type="number"
+            }else{
+                input.type="text"
+            }
+
+            formHtml.appendChild(input)
+        }
+
+        document.body.appendChild(formHtml)
+    }
+}
+
+const miFormEngine2= new FormEngine2()
+miFormEngine2.render()
